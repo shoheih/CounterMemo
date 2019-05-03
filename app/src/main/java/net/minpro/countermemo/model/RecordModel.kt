c9aa2f08ff5b5e4bfdcf9af379dd0478edad544b
shoheih <shoheigithub@gmail.com>
2019-05-03 23:59:24 +0900

    layout MainActivity and MasterFragment

--- app/src/main/java/net/minpro/countermemo/model/RecordModel.kt
+++ app/src/main/java/net/minpro/countermemo/model/RecordModel.kt
@@ -2,7 +2,7 @@ package net.minpro.countermemo.model
 
 import io.realm.RealmObject
 
-class RecordModel: RealmObject() {
+open class RecordModel: RealmObject() {
 
     //回数
     var count: Int = 0
--- app/src/main/java/net/minpro/countermemo/view/MainActivity.kt
+++ app/src/main/java/net/minpro/countermemo/view/MainActivity.kt
@@ -1,11 +1,9 @@
 package net.minpro.countermemo.view
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
 import kotlinx.android.synthetic.main.activity_main.*
 import net.minpro.countermemo.R
 
@@ -16,15 +14,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container_master, MasterFragment())
+                .commit()
         }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
+        menu.apply {
+            findItem(R.id.action_record).isVisible = true
+            findItem(R.id.action_regist).isVisible = true
+        }
         return true
     }
 
@@ -33,7 +36,8 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_record -> true
+            R.id.action_regist -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
