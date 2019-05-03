8d386886ad6053680931c84532641f64e605638e
shoheih <shoheigithub@gmail.com>
2019-05-04 00:38:34 +0900

    viewmodel and databinding

--- app/src/main/java/net/minpro/countermemo/view/MasterFragment.kt
+++ app/src/main/java/net/minpro/countermemo/view/MasterFragment.kt
@@ -1,12 +1,16 @@
 package net.minpro.countermemo.view
 
 
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import net.minpro.countermemo.R
+import net.minpro.countermemo.databinding.FragmentMasterBinding
+import net.minpro.countermemo.viewmodel.MainViewModel
 
 
 /**
@@ -15,13 +19,29 @@ import net.minpro.countermemo.R
  */
 class MasterFragment : Fragment() {
 
+    lateinit var viewModel: MainViewModel
+    private lateinit var binding: FragmentMasterBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_master, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_master, container, false)
+        val view = binding.root
+        return view
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(activity!!).get(MainViewModel::class.java)
 
+        binding.apply {
+            viewmodel = viewModel
+            setLifecycleOwner(activity)
+        }
+
+        viewModel.initParameters()
+
+    }
 }
---
+++ app/src/main/java/net/minpro/countermemo/viewmodel/MainViewModel.kt
@@ -0,0 +1,26 @@
+package net.minpro.countermemo.viewmodel
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+    var countNum = MutableLiveData<Int>()
+    var displayNum = MutableLiveData<String>()
+
+    fun initParameters() {
+        countNum.value = 0
+        displayNum.value = countNum.value.toString()
+    }
+
+    fun countUp() {
+        countNum.value = countNum.value!! + 1
+        displayNum.value = countNum.value.toString()
+    }
+
+    fun countDown() {
+        if (countNum.value!! <= 0) return
+        countNum.value = countNum.value!! - 1
+        displayNum.value = countNum.value.toString()
+    }
+}
